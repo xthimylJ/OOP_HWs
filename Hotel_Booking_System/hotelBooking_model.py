f1c310476737537c501fddbd3927083dbe37186c
xthimylJ <nazikudev@gmail.com>
2025-04-13 15:20:10 +0600

    c

--- Hotel_Booking_System/hotelBooking_model.py
+++ Hotel_Booking_System/hotelBooking_model.py
@@ -4,14 +4,17 @@ from entities_and_daos.customerDAO import CustomerDAO
  
 
 class Hotel_Booking_Model:
-    def __init__(self, customerDaoObj):
-        self.customerDaoObj = customerDaoObj
+    def __init__(self):
+        self.customerDaoObj = CustomerDAO()
 
-    def add_customer_to_db(self, customer):
+    def add_customer_to_db(self, customer:Customer):
+        
         self.customerDaoObj.insert(customer)
 
-    def show_all(self):
-        return self.customerDaoObj.fetch_all()
+    def show_all_customers(self):
+        customer_list = self.customerDaoObj.fetch_all()
+        return customer_list
+        
     
     
 
