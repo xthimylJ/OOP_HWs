626238d74131fb0f6267a469641557ff3b4633a1
xthimylJ <nazikudev@gmail.com>
2025-04-23 23:23:48 +0600

    c

---
+++ Abstaraction/shape.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+
+    
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+    def print_hw(self):
+        print("hello")
+
+    
+
+
+
